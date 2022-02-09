cd42ddc1683cc7d351aeb48e8992868ef771b3aa
M.M.Karmanova@urfu.me <M.M.Karmanova@urfu.me>
2022-02-09 16:00:45 +0500

    Добавьте файлы проекта.

---
+++ RevitAPI_App_6_3/Main.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Создайте приложение, которое позволяет расставлять определенное количество элементов
+//модели между двумя точками. Приложение должно работать так: приложение
+//запрашивает начальную и конечную точку. Далее появляется окно со списком типов
+//семейств и количество элементов, которое установится между указанными точками.
+
+
+namespace RevitAPI_App_6_3
+
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Внимание!", "Необходимо указать начальную и конечную точку, ограничивающих расстояние вставки элементов. " +
+                "Затем появится окно с выбором элементов");
+
+            var mainView = new MainView(commandData);
+            mainView.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+}
